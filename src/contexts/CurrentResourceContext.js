2c4822931f0ab04b6ee5296d54b05585ab12e3a2
Michael Scharff <minka.scharff@gmail.com>
2024-02-20 14:21:59 +0200

    Deleting all the unnecessary Contexts and Creating the necessary one

--- src/contexts/CurrentResourceContext.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-const CurrentResourceContext = React.createContext();
-export default CurrentResourceContext;
---
+++ src/contexts/MarkersContext.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const MarkersContext = React.createContext();
+export default MarkersContext;
