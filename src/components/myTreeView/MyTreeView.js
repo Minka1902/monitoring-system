15ba84fd0606ed2dde84658ca856d25acad00bf6
Michael Scharff <minka.scharff@gmail.com>
2024-02-20 14:27:43 +0200

    Organizing the code

--- src/components/myTreeView/MyTreeView.js
+++ src/components/myTreeView/MyTreeView.js
@@ -5,7 +5,7 @@ import { TreeView } from '@mui/x-tree-view/TreeView';
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
 import { formatName } from '../../constants/functions';
 
-export default function MyTreeView({ files, onElementClick }) {
+export default function MyTreeView({ files, onClick }) {
     return (
         <TreeView
             aria-label="file system navigator"
@@ -15,29 +15,27 @@ export default function MyTreeView({ files, onElementClick }) {
             sx={{ height: '100%', flexGrow: 1, maxWidth: 250, overflowY: 'auto' }}
         >
             {files !== undefined ? files.children.map((entry, index) => (
-                <TreeItem key={`${entry.name}${index}`} onClick={onElementClick} nodeId={entry.name} label={formatName(entry.name)}>
-                    <TreeElement key={`${entry.name}${index}`} entry={entry} onClick={onElementClick} />
+                <TreeItem className={entry.name} key={`${entry.name}${index}`} onClick={onClick} nodeId={entry.name} label={formatName(entry.name)}>
+                    <TreeElement key={`${entry.name}${index}`} entry={entry} onClick={onClick} />
                 </TreeItem>
             )) : <></>}
         </TreeView>
     );
-}
+};
 
 function TreeElement({ entry, onClick }) {
     return (
         entry !== undefined ?
             <div style={{ paddingLeft: `5px` }} >
                 {entry.children?.map((entry, index) => (
-                    entry.type === 'directory' && entry.children.length > 0 ?
-                        <TreeItem key={`${entry.name}${index}`} nodeId={entry.name} label={<div onClick={onClick}>{formatName(entry.name)}</div>}>
-                            <TreeElement entry={entry} onClick={onClick} />
-                        </TreeItem>
-                        :
-                        <TreeItem nodeId={`${entry.name}${index}`} key={`${entry.name}${index}`} label={<div onClick={onClick}>{formatName(entry.name)}</div>}>
-                        </TreeItem>
+                    <TreeItem key={`${entry.name}${index}`} onClick={onClick} nodeId={entry.name} className={entry.name} label={formatName(entry.name)}>
+                        {entry.type === 'directory' && entry.children.length > 0
+                            ? <TreeElement entry={entry} onClick={onClick} />
+                            : <></>}
+                    </TreeItem>
                 ))}
             </div>
             :
             <></>
     )
-}
+};
