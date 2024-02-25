ef6b1db7522fd77d555a5885604035b67d1f5b95
Michael Scharff <minka.scharff@gmail.com>
2024-02-25 14:33:12 +0200

    Adding the display of well names in the tree

--- src/components/myTreeView/MyTreeView.js
+++ src/components/myTreeView/MyTreeView.js
@@ -1,21 +1,22 @@
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import React from 'react';
+import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox';
 import DescriptionIcon from '@mui/icons-material/Description';
 import { TreeView } from '@mui/x-tree-view/TreeView';
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
 import { formatName } from '../../constants/functions';
+import DataContext from "../../contexts/DataContext";
 
 export default function MyTreeView({ files, onClick }) {
     return (
         <TreeView
             aria-label="file system navigator"
-            defaultCollapseIcon={<ExpandMoreIcon />}
-            defaultExpandIcon={<ChevronRightIcon />}
+            defaultCollapseIcon={<IndeterminateCheckBoxIcon />}
+            defaultExpandIcon={<IndeterminateCheckBoxIcon />}
             defaultEndIcon={<DescriptionIcon />}
             sx={{ height: '100%', flexGrow: 1, maxWidth: 250, overflowY: 'auto' }}
         >
             {files !== undefined ? files.children.map((entry, index) => (
-                <TreeItem className={entry.name} key={`${entry.name}${index}`} onClick={onClick} nodeId={entry.name} label={formatName(entry.name)}>
+                <TreeItem className={entry.name + " " + entry.type} key={`${entry.name}${index}`} onClick={onClick} nodeId={entry.name} label={formatName(entry.name)}>
                     <TreeElement key={`${entry.name}${index}`} entry={entry} onClick={onClick} />
                 </TreeItem>
             )) : <></>}
@@ -24,17 +25,28 @@ export default function MyTreeView({ files, onClick }) {
 };
 
 function TreeElement({ entry, onClick }) {
+    const wellsData = React.useContext(DataContext);
     return (
         entry !== undefined ?
             <div style={{ paddingLeft: `5px` }} >
                 {entry.children?.map((entry, index) => (
-                    <TreeItem key={`${entry.name}${index}`} onClick={onClick} nodeId={entry.name} className={entry.name} label={formatName(entry.name)}>
+                    <TreeItem key={`${entry.name}${index}`} onClick={onClick} nodeId={entry.name} className={entry.name + " " + entry.type} label={formatName(entry.name)}>
                         {entry.type === 'directory' && entry.children.length > 0
                             ? <TreeElement entry={entry} onClick={onClick} />
-                            : <></>}
+                            : entry.type === 'file' && wellsData ?
+                                wellsData[entry.name.slice(entry.name.lastIndexOf('-') + 1, -4)].map((well) => {
+                                    return (
+                                        <TreeItem key={well.name} nodeId={well.name} className='well' label={formatName(well.name)}>
+                                        </TreeItem>
+
+                                    );
+                                })
+                                :
+                                <></>}
                     </TreeItem>
-                ))}
-            </div>
+                ))
+                }
+            </div >
             :
             <></>
     )
