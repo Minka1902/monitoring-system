4d128b89c7d60112518dd6998244480473844fb5
Michael Scharff <minka.scharff@gmail.com>
2024-02-29 18:59:53 +0200

    Updating the page for using the graph data context

--- src/components/projectPlan/ProjectPlan.js
+++ src/components/projectPlan/ProjectPlan.js
@@ -1,10 +1,24 @@
 import React from "react";
+import GraphDataContext from '../../contexts/GraphDataContext';
+import DataContext from "../../contexts/DataContext";
 import { LinesChart } from "../chart/Charts";
 import { TableStandard, TableProgress } from '../table/Table';
 import { statusGraph, fieldsData, statusTable } from "../../constants/constants";
 import ProgressBar from '../progressBar/ProgressBar';
 
-export default function ProjectPlan({ data = '' }) {
+export default function ProjectPlan() {
+    const wellsData = React.useContext(DataContext);
+    const graphData = React.useContext(GraphDataContext);
+
+    const returnAvgProduction = () => {
+        if (wellsData) {
+            let total = 0;
+            for (let i = 0; i < wellsData.production.length; i++) {
+                total += parseFloat(wellsData.production[i].avgProd);
+            }
+            return total / wellsData.production.length;
+        }
+    };
 
     return (
         <section id="projectPlan">
@@ -12,25 +26,33 @@ export default function ProjectPlan({ data = '' }) {
                 <ProgressBar value={2015} maxValue={2024} minValue={1984} />
             </div>
             <div className="projectPlan__content__container">
-                <div className="projectPlan__resources-graph-container">
-                    <LinesChart data={statusGraph} title={false} />
+                <div className="projectPlan__resources-graph-container resources_graph">
+                    {graphData && graphData.resources_graph !== undefined ?
+                        <LinesChart data={graphData.resources_graph} title={false} /> :
+                        <></>}
                 </div>
                 <div className="projectPlan__fields-table-container">
                     <TableStandard tableHeaders={['Field', 'Object', 'Resources, min bbl']} data={fieldsData} color='#FEDCBA' />
                 </div>
-                <div className="projectPlan__state-graph-container">
-                    <LinesChart data={statusGraph} />
+                <div className="projectPlan__state-graph-container state_graph">
+                    {graphData && graphData.state_graph !== undefined ?
+                        <LinesChart data={graphData.state_graph} /> :
+                        <></>}
                 </div>
-                <div className="projectPlan__well__info">
-                    <p className="text">Wells in drilling: {data}</p>
-                    <p className="text">Wells in production: {data}</p>
-                    <p className="text">Average daily production: {data}</p>
-                    <p className="text">Other operations: {data}</p>
-                    <ul className="list">
-                        <li>Wells in testing - {data}</li>
-                        <li>Seismic survey - {data}</li>
-                    </ul>
-                </div>
+                <p className="projectPlan__well__info">
+                    Wells in drilling - {wellsData && wellsData.drilling ? wellsData.drilling.length : 'Please click a field/reservoir'}<br />
+                    Wells in production - {wellsData && wellsData.production && wellsData.production !== undefined ? wellsData.production.length : 'Please click a field/reservoir'}<br />
+                    Average production - {wellsData && wellsData.production && wellsData.production ? returnAvgProduction() : 'Please click a field/reservoir'} bbl/day<br />
+                    <span className="projectPlan__well_other-ops">
+                        Other operations:<br />
+                    </span>
+                    <span style={{ marginLeft: '15px' }}>
+                        Wells in testing - {wellsData && wellsData.test ? wellsData.test.length : 'Please click a field/reservoir'}<br />
+                    </span>
+                    <span className="projectPlan__well_survey" style={{ marginLeft: '15px' }}>
+                        Seismic survey - {false ? wellsData.threeDStatus : 'Can`t find status'}{<></>}
+                    </span>
+                </p>
                 <div className="projectPlan__status-table-container">
                     <TableProgress tableHeaders={[' ', ' ', 'Commitment', 'Actual', ' ']} data={statusTable} color='transparent' />
                 </div>
