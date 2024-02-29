f5bf007951b170cb944397131985719369541d03
Michael Scharff <minka.scharff@gmail.com>
2024-02-29 15:19:51 +0200

    Fixing some CSS and Adding the AreaChart the correct way

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,11 +3,11 @@ import Map from '../map/Map';
 import MarkersContext from "../../contexts/MarkersContext";
 import DataContext from "../../contexts/DataContext";
 import { getRandomInt } from "../../constants/functions";
-import { MainLinesChart, LinesChart, AreaChart } from '../chart/Charts';
+import { MainLinesChart, AreaChart } from '../chart/Charts';
 import proj4 from 'proj4';
 import epsg from 'epsg';
 
-export default function Main({ ROIData }) {
+export default function Main({ rOIData, mainGraphData }) {
     const [coords, setCoords] = React.useState([31.3, 34.8]);
     const markers = React.useContext(MarkersContext);
     const wellsData = React.useContext(DataContext);
@@ -24,19 +24,6 @@ export default function Main({ ROIData }) {
         center.y = center.y / markers.length;
         let coordinates = proj4(epsg['EPSG:2039'], epsg['EPSG:4326'], [parseFloat(center.x), parseFloat(center.y)]).reverse();
         setCoords(coordinates);
-    };
-
-    const createGraphData = (howMuchData) => {
-        let data = [];
-        for (let i = 0; i < howMuchData; i++) {
-            let obj = {};
-            obj.gas = getRandomInt(1500, 10000);
-            obj.oil = getRandomInt(900, 8000);
-            obj.water = getRandomInt(600, 6500);
-            obj.year = 2000 + i;
-            data[i] = obj;
-        }
-        return data;
     };
 
     const returnAvgProduction = () => {
@@ -61,14 +48,18 @@ export default function Main({ ROIData }) {
                 <div className="main__left__container">
                     <Map coords={coords} calcCenter={calculateMarkersCenter} />
                     <div className="main__resources_graph">
-                        <AreaChart chartData={ROIData} /><br /><br /><br />
+                        {
+                            rOIData !== undefined ?
+                                <AreaChart data={rOIData} /> : <></>
+                        }
+                        <br /><br /><br />
                         GIP/recoverable:<br />{getRandomInt(20, 250)}/{getRandomInt(20, 150)} MMcf<br /><br />
                         OIP/recoverable:<br />{getRandomInt(20, 250)}/{getRandomInt(20, 150)} MMbbl
                     </div>
                 </div>
                 <div className="main__right__container">
                     <div className="main__graph__container">
-                        <MainLinesChart data={createGraphData(25)} />
+                        <MainLinesChart data={mainGraphData} />
                     </div>
                     <p className="main__well__info">
                         Wells in drilling - {wellsData && wellsData.drilling ? wellsData.drilling.length : 'Please click a field/reservoir'}<br />
