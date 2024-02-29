c20a8d126768b41d1eaea7f72cfefbe1d39514b5
Michael Scharff <minka.scharff@gmail.com>
2024-02-29 09:17:11 +0200

    Creating the production page

--- src/components/production/Production.js
+++ src/components/production/Production.js
@@ -1,11 +1,31 @@
 import React from "react";
+import ProgressBar from '../progressBar/ProgressBar';
+import ButtonToggle from "../buttons/ButtonToggle";
+import { MainLinesChart, BubbleChart } from '../chart/Charts';
 
 export default function Production() {
     return (
         <section id="Production">
-            <div className="production__content__container">
-                <h2>Production content</h2>
+            <div className="production__time-bar">
+                <ProgressBar value={2015} maxValue={2024} minValue={1984} />
             </div>
-        </section >
+            <div className="production__content__container">
+                <div className="production__daily-prod_graph">
+                    <MainLinesChart />
+                </div>
+                <div className="production__well_stage">
+                    <img className="production__well_stage-image" src={require('../../images/wellStage.png')} alt="asdasd" />
+                </div>
+                <div className="production__bubble-graph">
+                    <BubbleChart />
+                    <div className="production__switch__container">
+                        <ButtonToggle />
+                    </div>
+                </div>
+                <div className="production__another-graph">
+                    Unknown graph
+                </div>
+            </div>
+        </section>
     );
 };
