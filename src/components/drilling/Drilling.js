d86ff9373606ddf32518c6ffd017d10e897c2b5f
Michael Scharff <minka.scharff@gmail.com>
2024-03-05 17:10:48 +0200

    Creating the page

--- src/components/drilling/Drilling.js
+++ src/components/drilling/Drilling.js
@@ -1,10 +1,66 @@
 import React from "react";
+import GraphDataContext from "../../contexts/GraphDataContext";
+import { DrillingLinesChart, } from '../chart/Charts';
+import ButtonToggle from "../buttons/ButtonToggle";
 
 export default function Drilling() {
+    const graphData = React.useContext(GraphDataContext);
+    const [is3D, setIs3D] = React.useState(false);
+
+    const toogle3D = () => {
+        setIs3D(!is3D);
+    };
+
     return (
         <section id="drilling">
             <div className="drilling__content__container">
-                <h2>Drilling content</h2>
+                <div className="drilling__content_left">
+                    <div className="drilling__content_top">
+                        <div className="drilling__state__info">
+                            <h4 className="drilling__state_text">
+                                Current state: drilling 8.5`` section <br />
+                                24 hours progress: 5 m
+                            </h4>
+                            <ul className="drilling__state_list">
+                                <span style={{ marginLeft: '-30px' }} className="drilling__state_text">
+                                    Last 24 hours operations:
+                                </span>
+                                <li>Drilling 8.5`` section (1792-1797 m)</li>
+                                <li>POOH</li>
+                                <li>Bit changing</li>
+                            </ul>
+                            <ul className="drilling__state_list">
+                                <span style={{ marginLeft: '-30px' }} className="drilling__state_text">
+                                    Next 72 hours operations:
+                                </span>
+                                <li>Drilling 8.5`` section</li>
+                                <li>Logging (1700-1850 m)</li>
+                                <li>Cementing</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="drilling__content_bottom">
+                        <div className="drilling__3d__picture">
+
+                        </div>
+                        <ButtonToggle states={['3D', 'Section']} onClick={toogle3D} />
+                    </div>
+                </div>
+                <div className="drilling__content_right">
+                    <div className="drilling__content_top days_depth">
+                        {graphData && graphData.days_depth ?
+                            < DrillingLinesChart data={graphData.days_depth} /> : <></>}
+                    </div>
+                    <div className="drilling__content_bottom">
+                        <div className="drilling__geo_stats las_doc">
+                            las graph
+                            {/* {graphData && graphData.las_doc
+                                ? <DrillingLasChart well={graphData.las_doc} />
+                                : <></>
+                            } */}
+                        </div>
+                    </div>
+                </div>
             </div>
         </section >
     );
