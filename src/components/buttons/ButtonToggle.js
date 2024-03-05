16726700484e4a52237a983b223ea73bb0bb922d
Michael Scharff <minka.scharff@gmail.com>
2024-03-05 16:02:56 +0200

    Making the button adjustable

--- src/components/buttons/ButtonToggle.js
+++ src/components/buttons/ButtonToggle.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 
-export default function ToggleSwitch() {
+export default function ToggleSwitch({ states = ['Current', 'Cumulative'], onClick = () => console.log('Toggle clicked') }) {
     const [isChecked, setIsChecked] = useState(false);
 
-    const handleToggle = () => {
+    const handleToggle = (evt) => {
         setIsChecked(!isChecked);
+        onClick(evt);
     };
 
     return (
         <div className={`toggle-switch ${isChecked ? 'checked' : ''}`}>
-            <span className="label-left">Current</span>
-            <label className="switch" onClick={handleToggle}>
+            <span className="label-left">{states[0]}</span>
+            <label className="switch" >
                 <input type="checkbox" checked={isChecked} onChange={handleToggle} />
                 <span className="slider" onClick={handleToggle}></span>
             </label>
-            <span className="label-right">Cumulative</span>
+            <span className="label-right">{states[1]}</span>
         </div>
     );
 };
