31760c65523b9a81da88768868145534f3017f4f
Michael Scharff <minka.scharff@gmail.com>
2024-03-11 13:10:52 +0200

    Creating the BubblePieChart component

---
+++ src/components/chart/BubblePieChart.js
@@ -0,0 +1,29 @@
+import { PieChart, Pie, Cell, Tooltip } from 'recharts';
+import React from 'react';
+
+export default function BubblePieChart({ well, myMap, tooltip__wrapper = 'tooltip__wrapper' }) {
+    return (
+        <div
+            style={{
+                position: 'absolute',
+                top: `${myMap && myMap.latLngToContainerPoint(well.location).y}px`,
+                left: `${myMap && myMap.latLngToContainerPoint(well.location).x}px`,
+                zIndex: 1000,
+            }}
+        >
+            <PieChart width={20} height={20}>
+                <Tooltip wrapperClassName={tooltip__wrapper} />
+                <Pie
+                    data={well.data}
+                    outerRadius={10}
+                    fill="#8884d8"
+                    dataKey="value"
+                >
+                    {well.data && well.data.map((segment, idx) => (
+                        <Cell key={`cell-${idx}`} fill={segment.fill} />
+                    ))}
+                </Pie>
+            </PieChart>
+        </div>
+    );
+};
