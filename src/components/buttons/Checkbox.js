cc88101ce52ab9e5d2be741cbd0de75214db20f3
Michael Scharff <minka.scharff@gmail.com>
2024-03-11 17:32:13 +0200

    Adding the onClick to the text

--- src/components/buttons/Checkbox.js
+++ src/components/buttons/Checkbox.js
@@ -14,7 +14,7 @@ export default function Checkbox({ name = 'checkbox', onClick = () => console.lo
             <svg viewBox="0 0 64 64" height="1.5em" width="1.5em" onClick={toggleChecked}>
                 <path className="checkbox-path" d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16" pathLength="575.0541381835938"></path>
             </svg>
-            <span className="checkbox-text">{text}</span>
+            <span onClick={toggleChecked} className="checkbox-text">{text}</span>
         </label>
     );
 };
