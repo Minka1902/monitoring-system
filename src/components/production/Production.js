07d52b67b4d14f9f5497e2c57488fa765449a58c
Michael Scharff <minka.scharff@gmail.com>
2024-03-14 13:03:52 +0200

    Started using pageData instead of graphData

--- src/components/production/Production.js
+++ src/components/production/Production.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GraphDataContext from "../../contexts/GraphDataContext";
+import PageDataContext from "../../contexts/PageDataContext";
 import ProgressBar from '../progressBar/ProgressBar';
 import ToggleSwitch from "../buttons/ButtonToggle";
 import { ProductionLinesChart, ProductionChart } from '../chart/Charts';
@@ -7,6 +8,68 @@ import MapWithOverlay from "../map/ProductionMap";
 
 export default function Production() {
     const graphData = React.useContext(GraphDataContext);
+    const pageData = React.useContext(PageDataContext);
+
+    const prepData = (data, propName = 'fluids', fromIndex = 0, toIndex = 300) => {
+        const dataKeys = Object.keys(data);
+        let arraysObject = {};
+        if (dataKeys.length >= 1) {
+            let tempArr = [];
+            for (let k = 0; k < dataKeys.length; k++) {
+                for (let i = fromIndex; i < toIndex; i++) {
+                    tempArr[i - fromIndex] = { day: i + 1, fluids: sumOfArray(createArray(data[dataKeys[k]], propName, i)), water: data[dataKeys[k]][i].water };
+                }
+                arraysObject[dataKeys[k]] = tempArr;
+            }
+        }
+        return averageOfArrays(arraysObject);
+    };
+
+    const sumOfArray = (arr) => {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    };
+
+    const createArray = (data, prop, untilIndex) => {
+        let tempArr = [];
+        for (let i = 0; i < untilIndex; i++) {
+            tempArr[i] = data[i][prop];
+        }
+        return tempArr;
+    };
+
+    const averageOfArrays = (objectOfArrays) => {
+        const wellNames = Object.keys(objectOfArrays);
+        if (wellNames.length === 1) {
+            let averagedArray = [];
+            for (let i = 0; i < objectOfArrays[wellNames[0]].length; i++) {
+                averagedArray[i] = { day: 0, fluids: 0, water: 0 };
+            }
+            for (let i = 0; i < wellNames.length; i++) {
+                for (let j = 0; j < objectOfArrays[wellNames[i]].length; j++) {
+                    for (const prop in objectOfArrays[wellNames[i]][j]) {
+                        averagedArray[j][prop] += objectOfArrays[wellNames[i]][j][prop];
+                    }
+                }
+            }
+            for (let i = 0; i < averagedArray.length; i++) {
+                for (const prop in averagedArray[i]) {
+                    averagedArray[i][prop] = averagedArray[i][prop] / wellNames.length
+                }
+            }
+            return averagedArray;
+        } else {
+            return objectOfArrays[wellNames[0]];
+        }
+    };
+
+    React.useEffect(() => {
+        if (pageData && pageData.prod_300)
+            prepData(pageData.prod_300);
+    }, [pageData]);
 
     return (
         <section id="Production">
@@ -14,9 +77,9 @@ export default function Production() {
                 <ProgressBar value={2015} maxValue={2024} minValue={1984} />
             </div>
             <div className="production__content__container">
-                <div className="production__daily-prod_graph daily_production">
-                    {graphData && graphData.daily_production !== undefined ?
-                        <ProductionLinesChart data={graphData.daily_production} /> :
+                <div className="production__daily-prod_graph prod_300">
+                    {pageData && pageData.prod_300 !== undefined ?
+                        <ProductionLinesChart data={prepData(pageData.prod_300)} /> :
                         <></>}
                 </div>
                 <div className="production__well_stage">
