a599b6fe2398d306702a26e49d3da28c5b0a92b8
Michael Scharff <minka.scharff@gmail.com>
2024-03-19 17:18:55 +0200

    Removing unnecassery code

--- src/components/navOverPage/NavOverPage.js
+++ src/components/navOverPage/NavOverPage.js
@@ -5,12 +5,11 @@ import geomageLogo from '../../images/geomageLogo.png';
 export default function NavOverPage({ pages, onClick }) {
     const history = useHistory();
     const [currentPage, setCurrentPage] = React.useState(history.location.pathname.slice(1, history.location.pathname.length) || 'main');
-    const pageResourcesNeeded = { main: ['polygons', 'prod_300'], production: ['prod_300'], drilling: [''], "project-plan": [''] };
 
     const onButtonClick = (evt) => {
         if (evt.target.innerHTML !== currentPage) {
             setCurrentPage(evt.target.innerHTML);
-            onClick(evt.target.innerHTML, pageResourcesNeeded[evt.target.innerHTML]);
+            onClick(evt.target.innerHTML);
         }
     };
 
