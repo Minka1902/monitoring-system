5f067b45d58ed3daae8b9041ddaeca3c630c31b4
Michael Scharff <minka.scharff@gmail.com>
2024-03-21 13:28:20 +0200

    Adding the StyledTreeItem

--- src/components/myTreeView/MyTreeView.js
+++ src/components/myTreeView/MyTreeView.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox';
 import DescriptionIcon from '@mui/icons-material/Description';
+import { styled } from '@mui/material/styles';
 import { TreeView } from '@mui/x-tree-view/TreeView';
-import { TreeItem } from '@mui/x-tree-view/TreeItem';
+import { TreeItem, treeItemClasses } from '@mui/x-tree-view/TreeItem';
 import { formatName } from '../../constants/functions';
 
-export default function MyTreeView({ files, onClick }) {
+const StyledTreeItem = styled(TreeItem)(() => ({
+    [`& .${treeItemClasses.content}`]: {
+        [`& .${treeItemClasses.label}`]: {
+            fontSize: '20px',
+        },
+    },
+}));
+
+export default function MyTreeView({ files, onClick, allFieldsClick }) {
     return (
         <TreeView
             aria-label="file system navigator"
             defaultCollapseIcon={<IndeterminateCheckBoxIcon />}
             defaultExpandIcon={<IndeterminateCheckBoxIcon />}
             defaultEndIcon={<DescriptionIcon />}
-            sx={{ height: '100%', flexGrow: 1, maxWidth: 250, overflowY: 'auto' }}
+            sx={{ height: '100%', flexGrow: 1, maxWidth: 350, overflowY: 'auto' }}
         >
-            <TreeItem nodeId="/forTreeView" className='forTreeView directory' label='All fields' onClick={onClick}>
+            <StyledTreeItem nodeId="/forTreeView" className='forTreeView directory' label='All fields' onClick={allFieldsClick}>
                 {files !== undefined ? files.children.map((entry, index) => (
-                    <TreeItem className={entry.name + " " + entry.type} key={`${entry.name}${index}`} onClick={onClick} nodeId={entry.path} label={formatName(entry.name)}>
+                    <StyledTreeItem className={entry.name + " " + entry.type} key={`${entry.name}${index}`} onClick={onClick} nodeId={entry.path} label={formatName(entry.name)}>
                         <TreeElement key={`${entry.name}${index}`} entry={entry} onClick={onClick} />
-                    </TreeItem>
+                    </StyledTreeItem>
                 )) : <></>}
-            </TreeItem>
+            </StyledTreeItem>
         </TreeView>
     );
 };
@@ -32,15 +41,15 @@ function TreeElement({ entry, onClick }) {
         entry !== undefined ?
             <div style={{ paddingLeft: `5px` }} >
                 {entry.children?.map((entry, index) => (
-                    <TreeItem key={`${entry.name}${index}`} onBlur={onBlurEvent} onClick={onClick} nodeId={entry.path ? entry.path : entry.name} className={entry.name + " " + entry.type} label={formatName(entry.name)}>
+                    <StyledTreeItem key={`${entry.name}${index}`} onBlur={onBlurEvent} onClick={onClick} nodeId={entry.path ? entry.path : entry.name} className={entry.name + " " + entry.type} label={formatName(entry.name)}>
                         {entry.children?.map((entry, index) => (
-                            <TreeItem key={`${entry.name}${index}`} onBlur={onBlurEvent} onClick={onClick} nodeId={entry.path ? entry.path : entry.name} className={entry.name + " " + entry.type} label={formatName(entry.name)}>
+                            <StyledTreeItem key={`${entry.name}${index}`} onBlur={onBlurEvent} onClick={onClick} nodeId={entry.path ? entry.path : entry.name} className={entry.name + " " + entry.type} label={formatName(entry.name)}>
                                 {entry.children.length > 0
                                     ? <TreeElement entry={entry} onClick={onClick} />
                                     : <></>}
-                            </TreeItem>
+                            </StyledTreeItem>
                         ))}
-                    </TreeItem>
+                    </StyledTreeItem>
                 ))}
             </div>
             :
