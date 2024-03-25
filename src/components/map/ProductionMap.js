f8dc5f4701353a5e32490435291d61de4694ffcb
Michael Scharff <minka.scharff@gmail.com>
2024-03-25 17:24:56 +0200

    Fixing the bubble chart data creation

--- src/components/map/ProductionMap.js
+++ src/components/map/ProductionMap.js
@@ -3,12 +3,14 @@ import 'leaflet/dist/leaflet.css';
 import epsg from 'epsg';
 import proj4 from 'proj4';
 import BubblePieChart from '../chart/BubblePieChart';
-import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
+import { MapContainer, TileLayer, useMapEvents, Polygon, useMap } from "react-leaflet";
 import React from 'react';
+import PageDataContext from "../../contexts/PageDataContext";
 import DataContext from "../../contexts/DataContext";
 
-const MapWithOverlay = () => {
+const MapWithOverlay = ({ polygons, polyName, isSum }) => {
     const mapRef = React.useRef();
+    const pageData = React.useContext(PageDataContext);
     const wellsData = React.useContext(DataContext);
     const center = { lat: 31.9, lng: 35 };
     const [tempChartData, setTempChartData] = React.useState(undefined);
@@ -56,34 +58,80 @@ const MapWithOverlay = () => {
     const epsgConvert = ({ x, y, toEpsg = 4326, fromEpsg = 2039 }) => {
         const toProj = epsg[`EPSG:${toEpsg}`];
         const fromProj = epsg[`EPSG:${fromEpsg}`];
-        let coordinates = proj4(fromProj, toProj, [parseFloat(x), parseFloat(y)]);
-        return { lat: parseFloat(coordinates[1].toFixed(5)), lng: parseFloat(coordinates[0].toFixed(5)) };
+        let coordinates = proj4(fromProj, toProj, [parseFloat(x), parseFloat(y)]).reverse();
+        return coordinates;
+    };
+
+    function createPolygon(polygonData) {
+        if (polygonData) {
+            let polygon = [];
+
+            for (let i = 0; i < polygonData.length; i++) {
+                polygon[i] = epsgConvert({ x: polygonData[i].x, y: polygonData[i].y });
+            };
+
+            return polygon;
+        }
+    };
+
+    const sumProp = (object, prop) => {
+        if (object && prop !== 'well') {
+            let sum = 0;
+            for (let i = 0; i < object.length; i++) {
+                if (object[i][prop] !== undefined) {
+                    sum += parseFloat(object[i][prop]);
+                }
+            }
+            return sum;
+        }
     };
 
     const createData = (wells) => {
         let newData = [];
         for (let i = 0; i < wells.length; i++) {
-            const coordinates = epsgConvert({ x: wells[i].x, y: wells[i].y })
-            const data = [{ name: 'water', value: parseFloat(wells[i].water), fill: 'blue' }, { name: 'oil', value: parseFloat(wells[i].oil), fill: 'black' }, { name: 'gas', value: parseFloat(wells[i].gas), fill: 'gray' }];
-            const tempD = { name: wells[i].name, location: coordinates, divPoint: { x: 100, y: 100 }, data };
-            newData[i] = tempD;
+            if (pageData.prod_300[wells[i].name] !== "File not found." && pageData.prod_300[wells[i].name] !== undefined) {
+                const coordinates = epsgConvert({ x: wells[i].x, y: wells[i].y });
+                if (isSum) {
+                    // ! here is sum
+                    let myObject = pageData && pageData.prod_300[wells[i].name];
+                    var data = [
+                        { name: 'water', value: Math.ceil(sumProp(myObject, 'water')), fill: 'blue' },
+                        { name: 'oil', value: Math.ceil(sumProp(myObject, 'oil')), fill: 'black' },
+                        { name: 'gas', value: Math.ceil(sumProp(myObject, 'gas')), fill: 'gray' }
+                    ];
+                } else {
+                    // ! here is not sum
+                    let myObject = pageData && pageData.prod_300[wells[i].name][pageData.prod_300[wells[i].name].length - 1];
+                    var data = [
+                        { name: 'water', value: parseFloat(myObject.water), fill: 'blue' },
+                        { name: 'oil', value: parseFloat(myObject.oil), fill: 'black' },
+                        { name: 'gas', value: parseFloat(myObject.gas), fill: 'gray' }
+                    ];
+                }
+                const tempD = { name: wells[i].name, location: coordinates, divPoint: { x: 100, y: 100 }, data };
+                newData.push(tempD);
+            }
         }
         return newData;
     };
 
     React.useEffect(() => {
-        if (wellsData && wellsData.production[0].oil) {
+        if (pageData && pageData.prod_300) {
             const asd = createData(wellsData.production);
             setChartData(asd);
         }
-    }, [wellsData]);
+    }, [pageData, wellsData, isSum]);
 
     return (
-        <MapContainer center={center} zoom={7} ref={mapRef} style={{ height: '500px', width: '100%' }} inertia={true}>
+        <MapContainer center={center} maxZoom={15} zoom={9} minZoom={7} ref={mapRef} style={{ height: '500px', width: '100%' }} inertia={true}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
+
+            {polygons && Object.keys(polygons).map((polygon, index) => {
+                return <Polygon positions={createPolygon(polygons[polygon])} className={polygon.slice(-2) === "3D" ? 'polygon_black' : 'polygon_blue'} key={index} />
+            })}
 
             <LocationFinder />
 
